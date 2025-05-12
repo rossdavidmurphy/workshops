1aea28c3262658c5f5febcd0d6ca10172f7029dc
Ross Murphy <rossdavidmurphy@gmail.com>
2025-05-12 13:38:38 -0400

    add dealership methods

--- CarDealership/src/main/java/com/pluralsight/DealershipFileManager.java
+++ CarDealership/src/main/java/com/pluralsight/DealershipFileManager.java
@@ -1,4 +1,13 @@
 package com.pluralsight;
 
 public class DealershipFileManager {
+
+    public Dealership getDealership() {
+        Dealership dealership = new Dealership("", "", "");
+        return dealership;
+    }
+
+    public void saveDealership(Dealership dealership) {
+        
+    }
 }
